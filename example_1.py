ba94631e6655789f5da4ec8450ca36efbb12ed40
cuiiim6 <cuiiim6@gmail.com>
2023-09-25 12:40:21 -0600

    PYTHON Y SUS LIBRERIAS

---
+++ example_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#by cuiiim6@gmail.com
+#GNU/GPL License
+
+import numpy as np #Usaremos nuestra primera biblioteca
+
+x = np.pi #Hacer una reserva de memoria sobre la RAM de un tipo de dato determinado.
+
+x = np.cos(x) #Puedes ahorrar memoria si uilizamos menos variables
+print(x)#A diferencia de otros lenguajes, en Python la memoria la reserva automáticamente.
+#Lenguaje prezoso, ejecuta las ordenes sólo cuando son necesarias.
+#Lenguaje No perezoso, ejecuta las ordenes activamente
+#(Python)Es un interpretado(No necesita compilador para ejecutarse, y es No perezoso)
